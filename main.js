027d61dfa107825318a329f03c90c0e7b8272f91
Rikki407 <rishablamba407@gmail.com>
2018-02-09 08:28:46 +0530

    UI updated

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var colorPalets = document.getElementsByClassName("colorPalets");
+var newColors = document.querySelector(".newColor");
+var ans;
+var ans_pos ;
 
-var ans  = "rgb("+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+")";
-var ans_pos = Math.floor(Math.random()*6);
-for(var i=0;i<colorPalets.length;i++)
-{
-    if(i===ans_pos){
-        colorPalets[i].style.backgroundColor = ans;
-    }
-    else {
-        colorPalets[i].style.backgroundColor = "rgb(" + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ")";
+function changePalets () {
+    ans  = "rgb("+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+")";
+    ans_pos = Math.floor(Math.random()*6);
+    console.log("Function called");
+    for(var i=0;i<colorPalets.length;i++)
+    {
+        if(i===ans_pos){
+            colorPalets[i].style.backgroundColor = ans;
+        }
+        else {
+            colorPalets[i].style.backgroundColor = "rgb(" + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ")";
+        }
     }
 }
-console.log("position"+ans_pos+"  :  "+ans);
+
+changePalets();
+
+newColors.addEventListener("click",changePalets);
