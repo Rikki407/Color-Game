823758efa85c9afca370679e8b80265ad9b6e6e3
Rikki407 <rishablamba407@gmail.com>
2018-02-09 16:31:54 +0530

    Game Logic Implemented

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var ans;
 var ans_pos ;
 var question = document.querySelector("#question");
 function changePalets () {
+    document.querySelector("#heading").style.backgroundColor = "#247BA0";
+    document.querySelector("#change_this").innerHTML = "<strong>&nbspNEW COLORS&nbsp</strong>";
+    document.querySelector(".correct").style.visibility = "hidden";
+
+
     ans  = "rgb("+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+", "+(Math.floor(Math.random()*256))+")";
     question.textContent = ans;
     ans_pos = Math.floor(Math.random()*6);
@@ -22,9 +27,17 @@ changePalets();
 for(var i=0;i<colorPalets.length;i++)
 {
     colorPalets[i].addEventListener('click',function () {
-        if(ans === this.style.backgroundColor)
-        {
-            console.log("You Found it!!!!");
+        if(ans === this.style.backgroundColor) {
+            document.querySelector("#heading").style.backgroundColor = ans;
+            document.querySelector(".correct").style.visibility = "visible";
+            document.querySelector("#change_this").innerHTML = "<strong>&nbspPLAY AGAIN?&nbsp</strong>";
+            for(var j=0;j<colorPalets.length;j++)
+            {
+                colorPalets[j].style.backgroundColor = ans;
+            }
+        }
+        else {
+            this.style.backgroundColor = "#232323";
         }
     })
 }
